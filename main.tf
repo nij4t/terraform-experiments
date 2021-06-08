103237f29f59759cf6cade76b787b025d2991f7d
Nijat Mahmudov <n.e.mahmudov@gmail.com>
2021-06-09 00:19:11 +0400

    always update resource
    Signed-off-by: Nijat Mahmudov <n.e.mahmudov@gmail.com>

--- main.tf
+++ main.tf
@@ -1,11 +1,19 @@
-module "config" {
+module "config_store" {
     source = "./modules/config-store"
 
     path = "./spec.yaml"
 }
 
+data "external" "uuid" {
+    program = ["./scripts/uuid.sh"]
+}
+
 resource "null_resource" "example" {
+
+    triggers = {
+        unique_id = data.external.uuid.result.value
+    }
     provisioner "local-exec" {
-        command = "echo '${module.config.config.spec.name}'"
+        command = "echo '${module.config_store.config.spec.name}'"
     }
 }
